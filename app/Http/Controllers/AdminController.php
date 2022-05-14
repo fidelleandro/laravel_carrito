c6008c55464205e727963c68b604cc8a1af8abc5
Fidel leandro Cerna Ruiz <fidelleandro@msn.com>
2022-05-13 22:14:49 -0500

    fix AdminController

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -21,12 +21,23 @@ class AdminController extends Controller
     public function dashboard(){
         return view('dashboard');
     }
-    public function page($page){ 
-        $privilegio = $this->getMethodControllerPage($page);
-        
+    public function page($page){ //crear-usuario
+        $privilegio = $this->getMethodControllerPriv($page);
+        if (count($privilegio)) {
+            session()->put('current_page',$page);
+            $ruta_privilegio = $this->getRouteAdmin();// Obtener la ruta del controlador o controladores de los privilegios
+            return app($ruta_privilegio.$privilegio['controller'])->$method();
+        }
+        else{
+            return view('dashboard.404');
+        }
         return view('dashboard');
     }
-    public function getMethodControllerPage($page){
+    public function getRouteAdmin(){
+        $ruta =  'App\Http\Controllers\Privilegios\.';
+        return substr($ruta,0,strlen($ruta)-1);
+    }
+    public function getMethodControllerPriv($page){
         $privilegio = new PrivilegioModel();
         $data = $privilegio::select(['name','controller','method','post_method','get_method'])->where('url',$page)->get()->toArray();
         $priv = isset($data[0]) ? $data[0] : array();
